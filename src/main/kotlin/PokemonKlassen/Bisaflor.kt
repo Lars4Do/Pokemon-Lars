bdf441d6e5d4924bc7819ec04a1b50c797ebc6e6
Lars4do <Lars.Hilgers95@gmail.com>
2023-09-18 13:56:30 +0200

    s

---
+++ src/main/kotlin/PokemonKlassen/Bisaflor.kt
@@ -0,0 +1,18 @@
+package PokemonKlassen
+
+import PokemonKlassen.Pokemon
+
+class Bisaflor():Pokemon(
+    "Bisaflor",
+    mutableListOf("Pflanze", "Gift"),
+    50,
+    "Leben-Orb",
+    "Chlorophyll",
+    mutableListOf("Solarstrahl", "Erdkräfte", "Matschbombe", "Schlafpuder"),
+    187,
+    147,
+    148,
+    167,
+    167,
+    145) {
+}
